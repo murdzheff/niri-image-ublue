2a5907ea0904555165450f2cf3b9e1ff41c0637b
murdzheff <murdzheff@protonmail.com>
2026-01-18 20:04:04 +0200

    initial

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,11 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install dms niri
+
+dnf5 -y copr disable avengemedia/dms
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
