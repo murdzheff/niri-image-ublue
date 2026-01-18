a73f4bb4bbb01599d5b069c98b2797769fe9c283
murdzheff <murdzheff@protonmail.com>
2026-01-18 20:53:15 +0200

    try noctalia

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,11 +19,12 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 -y copr enable avengemedia/dms
-dnf5 -y install dms niri xwayland-satellite swaybg swayidle grim slurp
+dnf5 -y install niri xwayland-satellite swaybg swayidle grim slurp
 
-dnf5 -y copr disable avengemedia/dms
 
+
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 #### Example for enabling a System Unit File
+dnf5 -y install noctalia-shell
 
 systemctl enable podman.socket
